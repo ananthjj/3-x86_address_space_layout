0edde576df7efc2d1edc3d4bfcd17c6ce698d339
ananth <ananth.josyula@gmail.com>
2022-09-20 22:47:29 -0500

    v3 Final Submission

--- segments.c
+++ segments.c
@@ -4,52 +4,48 @@
 #include <stdio.h>
 
 char* textFunc1(){
-  char* p = malloc(sizeof(char)*strlen("text 1"));
-  strcpy(p,"text 1");
-  return p;
+  return "text 1";
 }
 
 char* textFunc2(){
-  char* p = malloc(sizeof(char)*strlen("text 2"));
-  strcpy(p,"text 2");
-  return p;
-}
-
-char* stackFunc1(){
-  char* f1;
-  f1 = "stack 1";
-  return f1;
+  return "text 2";
 }
 
 char* stackFunc2(){
-  char* f2;
-  f2 =  "stack 2";
+  char* f2 = "stack 2";
   return f2;
+}
+
+char* stackFunc1(){
+  char* f1 = "stack 1";
+  stack_ptr_2 = stackFunc2();
+  return f1;
 }
 
 void init_pointers()
 {
-  char* temp1 = "heap 1";
-  char* temp2 = "heap 2";
-  heap_ptr_1 = malloc(sizeof(char)*strlen(temp1));
-  heap_ptr_2 = malloc(sizeof(char)*strlen(temp2));
-  strcpy(heap_ptr_1, temp1);
-  strcpy(heap_ptr_2, temp2);
-
-  static char* global1 = "data 1";
-  static char* global2 = "data 2";
-  data_ptr_1 = global1;
-  data_ptr_2 = global2;
-
-  bss_ptr_1 = "bss 1";
-  bss_ptr_2 = "bss 2";
-
   text_ptr_1 = &textFunc1;
   text_ptr_2 = &textFunc2;
-   
-  stack_ptr_1 = stackFunc1();
-  stack_ptr_2 = stackFunc2();
+
+  data_ptr_1 = "data 1";
+  data_ptr_2 = "data 2";
+
+  static char bss1[6];
+  strcpy(bss1, "bss 1");
+  bss_ptr_1 = bss1;
+
+  static char bss2[6];
+  strcpy(bss2, "bss 2");
+  bss_ptr_2 = bss2;
   
+  char* htemp1 = "heap 1";
+  char* htemp2 = "heap 2";
+  heap_ptr_1 = malloc(sizeof(char)*strlen(htemp1));
+  heap_ptr_2 = malloc(sizeof(char)*strlen(htemp2));
+  strcpy(heap_ptr_1, htemp1);
+  strcpy(heap_ptr_2, htemp2);
+
+  stack_ptr_1 = stackFunc1();
 }
 
 void free_pointers()
